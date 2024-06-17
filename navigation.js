edb1aab3c7a1f8c8db0ce8d4db4b7579357441b9
prince <princesoni200325@gmail.com>
2024-06-18 02:03:53 +0530

    Added liveliness to search button

--- navigation.js
+++ navigation.js
@@ -4,7 +4,7 @@ let scrollValue;
 let a = addEventListener("scroll",()=>{
     scrollValue = window.scrollY;
     if(scrollValue>50){
-        navBar.style.opacity = 0.6;
+        navBar.style.opacity = 0.4;
     } else {
         navBar.style.opacity = 1; 
     }
@@ -14,6 +14,15 @@ navBar.addEventListener("mouseenter",()=>{
 })
 navBar.addEventListener("mouseleave",()=>{
     if(scrollValue>50){
-        navBar.style.opacity = 0.6 
+        navBar.style.opacity = 0.4 
     }
 })
+// for search button
+let button = true;
+const searchButton = document.getElementById("submit_button");
+searchButton.onclick = ()=>{
+    searchButton.style.scale = .9;
+    setTimeout(()=>{
+        searchButton.style.scale = 1;
+    },110)
+}
