066da064bdb6c6aa92e22ebd3cc953397e3b510e
prince <princesoni200325@gmail.com>
2024-07-08 20:48:25 +0530

    transition effect for the button using js

--- navigation.js
+++ navigation.js
@@ -4,7 +4,7 @@ let scrollValue;
 let a = addEventListener("scroll",()=>{
     scrollValue = window.scrollY;
     if(scrollValue>50){
-        navBar.style.opacity = 0.4;
+        navBar.style.opacity = 0.6;
     } else {
         navBar.style.opacity = 1; 
     }
@@ -14,15 +14,30 @@ navBar.addEventListener("mouseenter",()=>{
 })
 navBar.addEventListener("mouseleave",()=>{
     if(scrollValue>50){
-        navBar.style.opacity = 0.4 
+        navBar.style.opacity = 0.6;
     }
 })
 // for search button
 let button = true;
 const searchButton = document.getElementById("submit_button");
+const applyButton = document.getElementById("ap_btn");
+const registerButton = document.getElementById("rg_btn");
 searchButton.onclick = ()=>{
     searchButton.style.scale = .9;
     setTimeout(()=>{
         searchButton.style.scale = 1;
+        // buttonAnime.style.scale = 1;
+    },70)
+}
+applyButton.onclick =()=>{
+    applyButton.style.scale = .9;
+    setTimeout(()=>{
+        applyButton.style.scale = 1;
+    },70)
+}
+registerButton.onclick =()=>{
+    registerButton.style.scale = .9;
+    setTimeout(()=>{
+        registerButton.style.scale = 1;
     },70)
 }
